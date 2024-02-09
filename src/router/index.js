667aa83b9b4e18a133153ebc9a0b35e926b5a787
ThePinkos <tomas.kubik2007@gmail.com>
2024-02-09 18:07:36 +0000

    HlavnStranka

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
-import { VueRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import Kalendar from '../Kalendar/StranaApp.vue'
+import Home from '../HlavnaStranka/HlavnaStranka.vue'
 
-const router = new VueRouter({
-  mode: 'history',
-  routes: []
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/kalendar',
+    name: 'kalendar',
+    component: Kalendar
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
 })
 
 export default router
